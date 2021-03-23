2c4880a3be488f00a135254e28ad4fa35c6333a0
agonzalezacevedo <agonzalezacevedo@danielcastelao.org>
2021-03-23 23:32:06 +0100

    models y demo actualizados

--- addons/openacademy/__manifest__.py
+++ addons/openacademy/__manifest__.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "openacademy",
+    'name': "Open Academy",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        Ejercicio creacion de modulo
     """,
 
-    'author': "My Company",
+    'author': "Alexandre",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Educacion',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
